45388f01021b51fb5db98d4e6f4708e7bb0c854b
chenk <hen.keinan@gmail.com>
2023-05-11 11:01:51 +0300

    refactor: cleanup and naming changes (#152)
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/bom/model.go
+++ pkg/bom/model.go
@@ -7,38 +7,25 @@ type Result struct {
 	NodesInfo  []NodeInfo  `json:"nodesInfo,omitempty"`
 }
 
-type Package struct {
-	ID         string     `json:",omitempty"`
-	Name       string     `json:",omitempty"`
-	Version    string     `json:",omitempty"`
-	Properties []KeyValue `json:",omitempty"`
-	Digest     string     `json:",omitempty"`
-}
-
-type KeyValue struct {
-	Name  string
-	Value string
-}
-
 type Component struct {
-	Type       string `json:"type,omitempty"`
-	ID         string `json:"id,omitempty"`
-	Version    string `json:"version,omitempty"`
-	Repository string `json:"repository,omitempty"`
-	Registry   string `json:"registry,omitempty"`
-	Digest     string `json:"digest,omitempty"`
+	Type       string
+	ID         string
+	Version    string
+	Repository string
+	Registry   string
+	Digest     string
 }
 
 type NodeInfo struct {
-	NodeRole                string   `json:"node_role,omitempty"`
-	NodeName                string   `json:"node_name,omitempty"`
-	KubeletVersion          string   `json:"kubelet_version,omitempty"`
-	ContainerRuntimeVersion string   `json:"container_runtime_version,omitempty"`
-	OsImage                 string   `json:"os_image,omitempty"`
-	Hostname                string   `json:"host_name,omitempty"`
-	KernelVersion           string   `json:"kernel_version,omitempty"`
-	KubeProxyVersion        string   `json:"kube_proxy_version,omitempty"`
-	OperatingSystem         string   `json:"operating_system,omitempty"`
-	Architecture            string   `json:"architecture,omitempty"`
-	Images                  []string `json:"images,omitempty"`
+	NodeRole                string
+	NodeName                string
+	KubeletVersion          string
+	ContainerRuntimeVersion string
+	OsImage                 string
+	Hostname                string
+	KernelVersion           string
+	KubeProxyVersion        string
+	OperatingSystem         string
+	Architecture            string
+	Images                  []string
 }

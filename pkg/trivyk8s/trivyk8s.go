2836f39ab7c8beba3566573df379b33ff509a7a5
chenk <hen.keinan@gmail.com>
2023-05-15 09:07:12 +0300

    feat: add component multi container
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- pkg/trivyk8s/trivyk8s.go
+++ pkg/trivyk8s/trivyk8s.go
@@ -189,7 +189,7 @@ func (c *client) ListBomInfo(ctx context.Context) ([]*artifacts.Artifact, error)
 		if err != nil {
 			return []*artifacts.Artifact{}, err
 		}
-		artifactList = append(artifactList, &artifacts.Artifact{Kind: "PodInfo", Name: c.ID, RawResource: rawResource})
+		artifactList = append(artifactList, &artifacts.Artifact{Kind: "PodInfo", Namespace: c.Namespace, Name: c.Name, RawResource: rawResource})
 	}
 	for _, ni := range bom.NodesInfo {
 		rawResource, err := rawResource(&ni)

3b6751b6787085a8b809afcc53ff8fd35d7bedac
chenk <hen.keinan@gmail.com>
2023-11-08 06:23:44 -0600

    test: ecr arn image ref
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- utils/referance_test.go
+++ utils/referance_test.go
@@ -6,7 +6,14 @@ import (
 )
 
 func TestParseRef(t *testing.T) {
-	imageRef, err := ParseReference("arn:aws:ecr:us-west-2:foo:repository/bar:latest")
-	assert.NoError(t, err)
-	assert.Equal(t, "repository/bar:latest", imageRef.String())
+	t.Run("parses imgae ref with arn", func(t *testing.T) {
+		imageRef, err := ParseReference("arn:aws:ecr:us-west-2:foo:repository/bar:latest")
+		assert.NoError(t, err)
+		assert.Equal(t, "repository/bar:latest", imageRef.String())
+	})
+	t.Run("parses imgae ref without arn", func(t *testing.T) {
+		imageRef, err := ParseReference("k8s.gcr.io/etcd:3.4.13-0")
+		assert.NoError(t, err)
+		assert.Equal(t, "k8s.gcr.io/etcd:3.4.13-0", imageRef.String())
+	})
 }

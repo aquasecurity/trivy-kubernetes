7c91619b134fed66a9add24ec8b738b35a305550
chenk <hen.keinan@gmail.com>
2024-07-15 17:05:26 +0300

    feat: ocp compliance support
    Signed-off-by: chenk <hen.keinan@gmail.com>

--- tests/integrations/integrations_test.go
+++ tests/integrations/integrations_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/aquasecurity/trivy-kubernetes/pkg/k8s"
 )
 
-func TestNodeInfo(T *testing.T) {
+func TestNodeInfoK8s(T *testing.T) {
 	ctx := context.Background()
 	if testing.Short() {
 		T.Skip("skipping end-to-end test")
@@ -25,7 +25,37 @@ func TestNodeInfo(T *testing.T) {
 	// collect node info
 	ar, err := trivyk8s.ListArtifactAndNodeInfo(ctx, []tk.NodeCollectorOption{
 		tk.WithScanJobNamespace("trivy-temp"),
-		tk.WithCommandPaths([]string{"./testdata"}),
+		tk.WithCommandPaths([]string{"./testdata/k8s"}),
+		tk.WithScanJobImageRef("ghcr.io/aquasecurity/node-collector:0.3.0"),
+	}...)
+	assert.NoError(T, err)
+	for _, a := range ar {
+		if a.Kind != "NodeInfo" {
+			continue
+		}
+		var expectedNodeIbfo map[string]interface{}
+		b, err := os.ReadFile("./testdata/expected_node_info.json")
+		assert.NoError(T, err)
+		err = json.Unmarshal(b, &expectedNodeIbfo)
+		assert.NoError(T, err)
+		assert.True(T, reflect.DeepEqual(expectedNodeIbfo["info"], a.RawResource["info"]))
+	}
+}
+
+func TestNodeInfoOcp(T *testing.T) {
+	ctx := context.Background()
+	if testing.Short() {
+		T.Skip("skipping end-to-end test")
+	}
+	cluster, err := k8s.GetCluster(k8s.WithBurst(100))
+	if err != nil {
+		panic(err)
+	}
+	trivyk8s := tk.New(cluster, tk.WithExcludeOwned(true))
+	// collect node info
+	ar, err := trivyk8s.ListArtifactAndNodeInfo(ctx, []tk.NodeCollectorOption{
+		tk.WithScanJobNamespace("trivy-temp"),
+		tk.WithCommandPaths([]string{"./testdata/ocp"}),
 		tk.WithScanJobImageRef("ghcr.io/aquasecurity/node-collector:0.3.0"),
 	}...)
 	assert.NoError(T, err)
